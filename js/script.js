a75f1c8bc2fc60013157642beee8fa53a54bf6dc
NataliaArsh <nata-arsh@mail.ru>
2021-09-27 17:53:52 +0900

    menu-burger

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// создание фона для хедера, когда страница скролится, на более чем 50 px,.. 
+//.. и отключение его при возвращении обратно в начало
 // создаю самовызывающуюся функцию
 // код внутри нее отработает сразу при загрузке script.js
 (function () {
@@ -9,5 +11,18 @@
             header.classList.remove('header_active');
         }
     };
+}());
+
+// открытие меню при нажатии на бургер
+(function () {
+    const burgerItem = document.querySelector('.burger');
+    const menu = document.querySelector('.header__menu');
+    const menuCloseItem = document.querySelector('.header__close');
+    burgerItem.addEventListener('click', () => {
+        menu.classList.add('header__menu-active');
+    });
+    menuCloseItem.addEventListener('click', () => {
+        menu.classList.remove('header__menu-active');
+    });
 }());
 
